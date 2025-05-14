aea5bc551e02fbf21dbe13005d238d89057e6b50
Himanshu <himbharti9902@gmail.com>
2025-05-14 12:06:49 +0530

    frontend url added

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ const jwt = require('jsonwebtoken');
 const app = express();
 const cors = require('cors');
 app.use(cors({
-    origin:'http://localhost:5173',
+    origin:'https://dev-bumble-frontend.vercel.app/',
     methods:['GET','POST','PATCH','DELETE'],
     allowedHeaders:['Content-Type','Authorization'],
     credentials:true
